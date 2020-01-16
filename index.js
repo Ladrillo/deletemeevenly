965cf23c92aef48bffe3d5f26e89f4044789059e
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-16 09:28:59 +0100

    all

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const express = require('express')
+const port = process.env.PORT || 4000
+const app = express()
+
+app.use(express.json())
+
+app.use((req, res) => {
+  res.json('the API is alive')
+})
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+})
