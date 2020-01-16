a6a2bf84c68475d4622e8a375e78e87d51b4b7bd
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-16 09:50:30 +0100

    all

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const port = process.env.PORT || 4000
 const app = express()
 
 app.use(express.static('client/build'))
 app.use(express.json())
+app.use(cors())
+
+app.get('/greeting', (req, res) => {
+  res.json(`Hey! how are you, ${req.query.name}?`)
+})
 
 app.use((req, res) => {
   res.json('the API is alive')
